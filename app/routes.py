9f2fda57bedbb7ef2ce87cfa1071930100b47cfc
Adrian Junus <adrian.junus@gmail.com>
2025-01-17 12:11:42 -0800

    homepage up

--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,22 @@
-from flask import render_template, request, redirect, url_for
-from . import db
-from .models import Post
+from flask import Blueprint, render_template, request, redirect, url_for
+from .models import Post, db
+from .__init__ import render_markdown
 
-@app.route('/')
+# Create a Blueprint for the routes
+main = Blueprint('main', __name__)
+
+@main.route('/')
 def index():
     posts = Post.query.all()
     return render_template('index.html', posts=posts)
 
-@app.route('/post/<int:id>')
+@main.route('/post/<int:id>')
 def view_post(id):
     post = Post.query.get_or_404(id)
+    post.content = render_markdown(post.content)
     return render_template('post.html', post=post)
 
-@app.route('/create', methods=['GET', 'POST'])
+@main.route('/create', methods=['GET', 'POST'])
 def create_post():
     if request.method == 'POST':
         title = request.form['title']
@@ -20,5 +24,5 @@ def create_post():
         new_post = Post(title=title, content=content)
         db.session.add(new_post)
         db.session.commit()
-        return redirect(url_for('index'))
+        return redirect(url_for('main.index'))
     return render_template('create.html')
